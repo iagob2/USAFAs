9171e9bfcaf65c79b6607a1bf3b0abd016397aa7
DutraGames <gabrieldutraamarante@gmail.com>
2024-10-28 11:17:28 -0300

    feat: add urls in urls.py

--- protoDigital/urls.py
+++ protoDigital/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path('registro/', views.Registro, name='registro'),
     path('login/', views.login, name='login'),
+    path('configuracoes/', views.Configuracoes, name='configuracoes'),
+    path('cartao_virtual/', views.Cartao_Virtual, name='cartao_virtual'),
+    path('marcar_consulta/', views.Marcar_Consulta, name='marcar_consulta'),
+    path('perfil_do_usuario/', views.Perfil_do_Usuario, name='perfil_do_usuario'),
 ]
