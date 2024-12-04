faa2605c903845a74fc973bc65ea44e46da88596
DutraGames <gabrieldutraamarante@gmail.com>
2024-12-04 09:30:30 -0300

    fix: ALLOWED_HOSTS permission

--- usafa/settings.py
+++ usafa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2k9842rsp+#ego5trcpdvc5m25xj(bbq(y5lwbwx^f9ip*sz9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
